e5584b0ab8c75172c8ac75f8bb4ea795e3ce2332
João Pedro Nascimento <jp.nascimento10@hotmail.com>
2022-06-27 13:54:38 -0300

    drawing graph without loop

--- teste.py
+++ teste.py
@@ -1,3 +1,9 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+
+
 def criaGrafo(sequencia1, sequencia2):
 
     grafo = {"": {}}
@@ -71,4 +77,41 @@ def criaGrafo(sequencia1, sequencia2):
     return grafo
 
 
-print(criaGrafo('cck', 'ckc'))
+grafo = (criaGrafo('cck', 'ckc'))
+
+for node in grafo.keys():
+    G.add_node(node)
+
+
+plt.figure(2)
+for node in grafo.keys():
+    if 'c' in grafo[node].keys():
+        G.add_edge(node, grafo[node]['c'])
+    if 'k' in grafo[node].keys():
+        G.add_edge(node, grafo[node]['k'])
+
+nx.draw(G, pos=nx.circular_layout(G), with_labels=True)
+plt.show()
+
+
+# print(G)
+
+
+# def calculaProb(state, sequencia1, sequencia2):
+
+#     if state == sequencia1:
+#         return 1
+
+#     if state == sequencia2:
+#         return 0
+
+#     if grafo[state]['c'] == state:
+#         return calculaProb(grafo[state]['k'], sequencia1, sequencia2)
+
+#     elif grafo[state]['k'] == state:
+#         return calculaProb(grafo[state]['c'], sequencia1, sequencia2)
+
+#     return 0.5 * (calculaProb(grafo[state]['c'], sequencia1, sequencia2) + calculaProb(grafo[state]['k'], sequencia1, sequencia2))
+
+
+# print(calculaProb('', 'cck', 'ckc'))

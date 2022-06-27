4cd5236aaeb96def102f594c78af0c6220fd7a28
João Pedro Nascimento <jp.nascimento10@hotmail.com>
2022-06-26 21:27:41 -0300

    criacao do grafo completa

--- Lógica/simuladorV2.py
+++ Lógica/simuladorV2.py
@@ -112,13 +112,13 @@ def criaGrafo(sequencia1, sequencia2):
 
         i += 1
 
-    j = 0
+    i = 0
 
     while sequencia2 not in grafo.keys():
 
-        current_state = sequencia2[:j]
+        current_state = sequencia2[:i]
 
-        choice = sequencia2[j:][0]
+        choice = sequencia2[i:][0]
 
         next_state = current_state + choice
 
@@ -127,7 +127,39 @@ def criaGrafo(sequencia1, sequencia2):
         if next_state not in grafo.keys():
             grafo[next_state] = {}
 
-        j += 1
+        i += 1
+
+    def checkPrevStateExist(origin, state):
+
+        if state == '':
+            if 'c' not in grafo[origin].keys():
+                grafo[origin]['c'] = ''
+            elif 'k' not in grafo[origin].keys():
+                grafo[origin]['k'] = ''
+
+        if state not in grafo.keys():
+            checkPrevStateExist(origin, state[1:])
+
+        else:
+            if 'c' not in grafo[state].keys():
+                target_state = (state + 'c')[1:]
+                if target_state in grafo.keys():
+                    grafo[origin]['c'] = target_state
+                else:
+                    checkPrevStateExist(origin, target_state[1:])
+
+            elif 'k' not in grafo[state].keys():
+                target_state = (state + 'k')[1:]
+                if target_state in grafo.keys():
+                    grafo[origin]['k'] = target_state
+                else:
+                    checkPrevStateExist(origin, target_state[1:])
+
+    for state in grafo.keys():
+
+        if state not in [sequencia1, sequencia2]:
+
+            checkPrevStateExist(state, state)
 
     return grafo
 
